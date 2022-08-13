a6d4b99f97d659468ee1afdeea27f55254016874
RevDonz <aprilio842@gmail.com>
2022-08-13 18:47:54 +0700

    feat(navbar): done create navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,8 +1,18 @@
+import clsx from 'clsx';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
+import { HiSun, HiMoon } from 'react-icons/hi';
+import Button from '@/components/atoms/Button';
 
 const Navbar = () => {
   const { pathname } = useRouter();
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => setMounted(true), []);
+  if (!mounted) return null;
 
   const Routes = [
     {
@@ -24,17 +34,49 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className='w-full sticky top-0 z-50'>
-      <div className='flex max-w-5xl mx-auto items-center gap-2 h-16'>
-        {Routes.map((route) => {
-          return (
-            <Link key={route.name} href={route.link}>
-              <a className='py-2 px-4 hover:bg-red-100 dark:hover:bg-dark-500/50 rounded-md'>
-                {route.name}
-              </a>
-            </Link>
-          );
-        })}
+    <nav
+      className={clsx('fixed inset-0 z-50 h-16', 'bg-zinc-50 dark:bg-dark-900')}
+    >
+      <div
+        className={clsx(
+          'layout gap-2 h-full',
+          'flex items-center justify-between'
+        )}
+      >
+        <div className='flex'>
+          {Routes.map((route) => {
+            return (
+              <Link key={route.name} href={route.link}>
+                <a
+                  className={clsx(
+                    'py-2 px-4 rounded-md transition-all font-semibold',
+                    'hover:bg-indigo-100 dark:hover:bg-dark-500/50',
+                    route.link === pathname
+                      ? 'text-indigo-700 dark:text-indigo-400'
+                      : 'text-gray-700 dark:text-gray-300'
+                  )}
+                >
+                  {route.name}
+                </a>
+              </Link>
+            );
+          })}
+        </div>
+        <Button
+          className={clsx(
+            'text-lg rounded-md h-10 w-10',
+            'bg-indigo-50 dark:bg-dark-500/50'
+          )}
+          onClick={() => {
+            setTheme(theme === 'light' ? 'dark' : 'light');
+          }}
+        >
+          {theme === 'light' ? (
+            <HiMoon className={clsx('text-indigo-700')} />
+          ) : (
+            <HiSun className={clsx('text-yellow-400')} />
+          )}
+        </Button>
       </div>
     </nav>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,9 @@
-import { useTheme } from 'next-themes';
-import { useEffect, useState } from 'react';
-import Main from '@/components/layouts/Main';
+import Main from '@/components/layout';
 
 export default function Index() {
-  const [mounted, setMounted] = useState(false);
-  const { theme, setTheme } = useTheme();
-
-  useEffect(() => setMounted(true), []);
-  if (!mounted) return null;
-
   return (
     <Main>
-      <div className=''>
-        <p>Current theme : {theme}</p>
-        <button
-          className='mt-16 px-4 py-2 text-white dark:text-black bg-black dark:bg-white font-semibold rounded-md'
-          onClick={() => {
-            setTheme(theme === 'light' ? 'dark' : 'light');
-          }}
-        >
-          Change Theme
-        </button>
-      </div>
+      <div className='h-screen mt-20'>asd</div>
     </Main>
   );
 }
