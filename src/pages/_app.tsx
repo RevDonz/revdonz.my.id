95d8b34ca99b84ab88f7da46cba2f5e75389ab47
RevDonz <aprilio842@gmail.com>
2022-08-13 15:20:15 +0700

    refactor(directory): create base url

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import '../styles/globals.css';
+import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import Navbar from '../components/navbar';
+import Navbar from '@/components/navbar';
 import { ThemeProvider } from 'next-themes';
 
 function MyApp({ Component, pageProps }: AppProps) {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { useTheme } from 'next-themes';
 import { useEffect, useState } from 'react';
-import Main from '../components/layouts/Main';
+import Main from '@/components/layouts/Main';
 
 export default function Index() {
   const [mounted, setMounted] = useState(false);
@@ -13,7 +13,7 @@ export default function Index() {
 
   return (
     <Main>
-      <div className=''>
+      <div className='font-primary'>
         <p>Current theme : {theme}</p>
         <button
           className='mt-16 px-4 py-2 text-white dark:text-black bg-black dark:bg-white font-semibold rounded-md'
