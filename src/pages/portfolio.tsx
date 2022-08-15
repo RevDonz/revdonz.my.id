c4fd7f200f72f02c8ea7492d94b8d58390f4f86f
RevDonz <aprilio842@gmail.com>
2022-08-15 19:21:40 +0700

    refactor(card): make card more usability

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -4,15 +4,8 @@ import clsx from 'clsx';
 import React from 'react';
 
 const portfolio = () => {
-  return (
-    <Main className='mt-10'>
-      <Hero
-        title={'Portfolio'}
-        description={
-          'Projek yang saya buat dengan semua pengetahuan dan pengalaman saya.'
-        }
-      />
-
+  const Card = ({ children }) => {
+    return (
       <div className='relative block group mt-10 active:scale-95'>
         <span
           className={clsx(
@@ -23,17 +16,39 @@ const portfolio = () => {
 
         <div className='p-0.5 relative h-full transition-transform transform group-hover:-translate-x-2 group-hover:-translate-y-2 bg-black dark:bg-gradient-to-r from-primary-500 via-ternary-500 to-secondary-500'>
           <div className='p-3 transition-opacity dark:bg-dark-900 bg-gray-50'>
-            <h2 className='mt-4 text-2xl font-medium'>Go around the world</h2>
-
-            <p className='mt-4'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Cupiditate, praesentium voluptatem omnis atque culpa repellendus.
-            </p>
-
-            <p className='mt-8 font-bold'>Read more</p>
+            {children}
           </div>
         </div>
       </div>
+    );
+  };
+
+  const ProjectCard = () => {
+    return (
+      <div className='flex flex-col'>
+        <h2 className='mt-4 text-2xl font-medium'>Go around the world</h2>
+
+        <p className='mt-4'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate,
+          praesentium voluptatem omnis atque culpa repellendus.
+        </p>
+
+        <p className='mt-8 font-bold'>Read more</p>
+      </div>
+    );
+  };
+  return (
+    <Main className='mt-10'>
+      <Hero
+        title={'Portfolio'}
+        description={
+          'Projek yang saya buat dengan semua pengetahuan dan pengalaman saya.'
+        }
+      />
+
+      <Card>
+        <ProjectCard />
+      </Card>
     </Main>
   );
 };
