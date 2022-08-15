a471aa76843b87dc3dc1f30c2370f24da22feb42
RevDonz <aprilio842@gmail.com>
2022-08-15 20:06:15 +0700

    fix(hero): fix types

--- src/components/mollecules/Hero.tsx
+++ src/components/mollecules/Hero.tsx
@@ -1,4 +1,11 @@
-const Hero = ({ title, description }) => {
+export interface HeroProps {
+  title: string;
+  description: string;
+  // children?: React.ReactNode;
+  // className?: string;
+}
+
+const Hero = ({ title, description }: HeroProps) => {
   return (
     <section>
       <h1>{title}</h1>
