60dc06a25a1807081c10b68a9e003a32dddd524e
RevDonz <aprilio842@gmail.com>
2022-08-15 23:46:13 +0700

    feat(about): add about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,9 +1,13 @@
+import Hero from '@/components/mollecules/Hero';
+import Main from '@/components/template'
 import React from 'react'
 
 const about = () => {
   return (
-    <div className=''>about</div>
-  )
+    <Main className='mt-10'>
+      <Hero title={'About Me'} description={'Ini adalah halaman tentang saya'} />
+    </Main>
+  );
 }
 
 export default about
