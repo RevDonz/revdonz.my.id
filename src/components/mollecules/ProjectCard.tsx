0c0a45279e23ce4a6d1f9ca4ed23625d489ec169
RevDonz <aprilio842@gmail.com>
2022-08-17 17:21:59 +0700

    feat(link): add unstyled and underlined link

--- src/components/mollecules/ProjectCard.tsx
+++ src/components/mollecules/ProjectCard.tsx
@@ -1,5 +1,6 @@
 import { ProjectProps } from '@/data/project';
-import UnstyledLink from '../atoms/UnstyledLink';
+import UnderlinedLink from './UnderlinedLink';
+import { FaGithub } from 'react-icons/fa';
 
 const ProjectCard = ({ title, summary, link }: ProjectProps) => {
   return (
@@ -9,10 +10,15 @@ const ProjectCard = ({ title, summary, link }: ProjectProps) => {
       <p className='my-4'>{summary}</p>
 
       <div className='flex mt-auto gap-4'>
-        {/* <UnstyledLink href={link.github}>Repository</UnstyledLink>
-        <UnstyledLink href={link.demo}>Demo</UnstyledLink> */}
-        <UnstyledLink href={''}>Repository</UnstyledLink>
-        <UnstyledLink href={''}>Demo</UnstyledLink>
+        {/* <UnderlinedLink href={link.github}>Repository</UnderlinedLink>
+        <UnderlinedLink href={link.demo}>Demo</UnderlinedLink> */}
+        <UnderlinedLink href={''}>
+          <div className="flex items-center gap-1">
+            <FaGithub />
+            <p>Repository</p>
+          </div>
+        </UnderlinedLink>
+        <UnderlinedLink href={''}>Demo</UnderlinedLink>
       </div>
     </div>
   );
