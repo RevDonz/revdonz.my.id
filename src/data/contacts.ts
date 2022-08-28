37fd625dcf86a1c2d2bd221529ade7cd3d72820f
RevDonz <aprilio842@gmail.com>
2022-08-28 12:39:16 +0700

    refactor(about): edit desc

---
+++ src/data/contacts.ts
@@ -0,0 +1,22 @@
+export const Contacts = [
+  {
+    type: 'Email',
+    link: 'mailto:aprilio842@gmail.com',
+  },
+  {
+    type: 'Github',
+    link: 'https://github.com/RevDonz',
+  },
+  {
+    type: 'LinkedIn',
+    link: 'https://www.linkedin.com/in/revdonz',
+  },
+  {
+    type: 'Instagram',
+    link: 'https://www.instagram.com/revdonz_',
+  },
+  {
+    type: 'Telegram',
+    link: 'https://t.me/RevDonz',
+  },
+];
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,44 +3,20 @@ import UnstyledLink from '@/components/atoms/UnstyledLink';
 import Hero from '@/components/mollecules/Hero';
 import HeroWithPhoto from '@/components/mollecules/HeroWithPhoto';
 import Main from '@/components/template';
-import clsx from 'clsx';
-import Link from 'next/link';
+import { Contacts } from '@/data/contacts';
 import React from 'react';
-import { HiChevronDown } from 'react-icons/hi';
 
 const about = () => {
-  const Contacts = [
-    {
-      type: 'Email',
-      link: 'mailto:aprilio842@gmail.com',
-    },
-    {
-      type: 'Github',
-      link: 'https://github.com/RevDonz',
-    },
-    {
-      type: 'LinkedIn',
-      link: 'https://www.linkedin.com/in/revdonz/',
-    },
-    {
-      type: 'Instagram',
-      link: 'https://www.instagram.com/revdonz_/',
-    },
-  ];
-
   return (
     <Main className='mt-10'>
       <Head title='About' />
-      <HeroWithPhoto
-        title={'About Me'}
-        subtitle='Reva Doni Aprilio'
-      >
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Totam
-          repellat dolore, repudiandae necessitatibus accusantium ut, explicabo
-          quam dignissimos laborum temporibus nostrum sapiente dolorem aliquam
-          distinctio ea quas animi quis ab.
+      <HeroWithPhoto title={'About Me'} subtitle='Reva Doni Aprilio'>
+        <p className='mb-3'>
+          {
+            "I'm a Frontend Developer and Software Engineering Student at Telkom University."
+          }
         </p>
+        <p></p>
       </HeroWithPhoto>
       <Hero
         title='Contact'
@@ -53,7 +29,9 @@ const about = () => {
                 <li key={index}>
                   <p className='truncate my-2'>
                     {contact.type} -{' '}
-                    <Link href={contact.link}>{contact.link}</Link>
+                    <UnstyledLink href={contact.link}>
+                      {contact.link.replace('mailto:', '')}
+                    </UnstyledLink>
                   </p>
                 </li>
               );
